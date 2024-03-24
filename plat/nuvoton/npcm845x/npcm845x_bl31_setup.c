e53b6af0610687f95120aa4814f39258f54bdb51
rutigl@gmail.com <rutigl@gmail.com>
2024-03-24 13:48:25 +0200

    remove unused define and print

--- plat/nuvoton/npcm845x/npcm845x_bl31_setup.c
+++ plat/nuvoton/npcm845x/npcm845x_bl31_setup.c
@@ -121,8 +121,8 @@ int board_uart_init(void)
 unsigned int plat_get_syscnt_freq2(void)
 {
 	/*
-	 * because BootBlock sets the correct frequency based on PLL
-	*/
+	 * to not overwrite the value set by BootBlock
+	 */
 	return (unsigned int)read_cntfrq_el0();
 }
 
@@ -354,6 +354,5 @@ void __init npcm845x_bl31_plat_arch_setup(void)
 		{0}
 	};
 	setup_page_tables(bl_regions, plat_arm_get_mmap());
-	enable_mmu_el3(0U);
-	NOTICE("Done enabling MMU\n");
+	enable_mmu_el3(0U);	
 }
