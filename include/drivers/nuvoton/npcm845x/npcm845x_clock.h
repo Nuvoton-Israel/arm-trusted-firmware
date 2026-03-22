329701f3f9ed7682ae28b79c6c28f4067f0281a4
fishav <fishav@nuvoton.com>
2026-03-22 18:00:29 +0200

    add to registers a comment about offset and absolute address

--- include/drivers/nuvoton/npcm845x/npcm845x_clock.h
+++ include/drivers/nuvoton/npcm845x/npcm845x_clock.h
@@ -7,52 +7,53 @@
 #ifndef __ARBEL_CLOCK_H_
 #define __ARBEL_CLOCK_H_
 
+/* CLK_BA = 0xF0801000 */
 struct clk_ctl {
-	unsigned int	clken1;
-	unsigned int	clksel;
-	unsigned int	clkdiv1;
-	unsigned int	pllcon0;
-	unsigned int	pllcon1;
-	unsigned int	swrstr;
-	unsigned char	res1[0x8];
-	unsigned int	ipsrst1;
-	unsigned int	ipsrst2;
-	unsigned int	clken2;
-	unsigned int	clkdiv2;
-	unsigned int	clken3;
-	unsigned int	ipsrst3;
-	unsigned int	wd0rcr;
-	unsigned int	wd1rcr;
-	unsigned int	wd2rcr;
-	unsigned int	swrstc1;
-	unsigned int	swrstc2;
-	unsigned int	swrstc3;
-	unsigned int	tiprstc;
-	unsigned int	pllcon2;
-	unsigned int	clkdiv3;
-	unsigned int	corstc;
-	unsigned int	pllcong;
-	unsigned int	ahbckfi;
-	unsigned int	seccnt;
-	unsigned int	cntr25m;
-	unsigned int	clken4;
-	unsigned int	ipsrst4;
-	unsigned int	busto;
-	unsigned int	clkdiv4;
-	unsigned int	wd0rcrb;
-	unsigned int	wd1rcrb;
-	unsigned int	wd2rcrb;
-	unsigned int	swrstc1b;
-	unsigned int	swrstc2b;
-	unsigned int	swrstc3b;
-	unsigned int	tiprstcb;
-	unsigned int	corstcb;
-	unsigned int	ipsrstdis1;
-	unsigned int	ipsrstdis2;
-	unsigned int	ipsrstdis3;
-	unsigned int	ipsrstdis4;
-	unsigned char	res2[0x10];
-	unsigned int	thrtl_cnt;
+	unsigned int	clken1;		/* offset: 0x00, addr: 0xF0801000 */
+	unsigned int	clksel;		/* offset: 0x04, addr: 0xF0801004 */
+	unsigned int	clkdiv1;	/* offset: 0x08, addr: 0xF0801008 */
+	unsigned int	pllcon0;	/* offset: 0x0C, addr: 0xF080100C */
+	unsigned int	pllcon1;	/* offset: 0x10, addr: 0xF0801010 */
+	unsigned int	swrstr;		/* offset: 0x14, addr: 0xF0801014 */
+	unsigned char	res1[0x8];	/* offset: 0x18, addr: 0xF0801018 */
+	unsigned int	ipsrst1;	/* offset: 0x20, addr: 0xF0801020 */
+	unsigned int	ipsrst2;	/* offset: 0x24, addr: 0xF0801024 */
+	unsigned int	clken2;		/* offset: 0x28, addr: 0xF0801028 */
+	unsigned int	clkdiv2;	/* offset: 0x2C, addr: 0xF080102C */
+	unsigned int	clken3;		/* offset: 0x30, addr: 0xF0801030 */
+	unsigned int	ipsrst3;	/* offset: 0x34, addr: 0xF0801034 */
+	unsigned int	wd0rcr;		/* offset: 0x38, addr: 0xF0801038 */
+	unsigned int	wd1rcr;		/* offset: 0x3C, addr: 0xF080103C */
+	unsigned int	wd2rcr;		/* offset: 0x40, addr: 0xF0801040 */
+	unsigned int	swrstc1;	/* offset: 0x44, addr: 0xF0801044 */
+	unsigned int	swrstc2;	/* offset: 0x48, addr: 0xF0801048 */
+	unsigned int	swrstc3;	/* offset: 0x4C, addr: 0xF080104C */
+	unsigned int	tiprstc;	/* offset: 0x50, addr: 0xF0801050 */
+	unsigned int	pllcon2;	/* offset: 0x54, addr: 0xF0801054 */
+	unsigned int	clkdiv3;	/* offset: 0x58, addr: 0xF0801058 */
+	unsigned int	corstc;		/* offset: 0x5C, addr: 0xF080105C */
+	unsigned int	pllcong;	/* offset: 0x60, addr: 0xF0801060 */
+	unsigned int	ahbckfi;	/* offset: 0x64, addr: 0xF0801064 */
+	unsigned int	seccnt;		/* offset: 0x68, addr: 0xF0801068 */
+	unsigned int	cntr25m;	/* offset: 0x6C, addr: 0xF080106C */
+	unsigned int	clken4;		/* offset: 0x70, addr: 0xF0801070 */
+	unsigned int	ipsrst4;	/* offset: 0x74, addr: 0xF0801074 */
+	unsigned int	busto;		/* offset: 0x78, addr: 0xF0801078 */
+	unsigned int	clkdiv4;	/* offset: 0x7C, addr: 0xF080107C */
+	unsigned int	wd0rcrb;	/* offset: 0x80, addr: 0xF0801080 */
+	unsigned int	wd1rcrb;	/* offset: 0x84, addr: 0xF0801084 */
+	unsigned int	wd2rcrb;	/* offset: 0x88, addr: 0xF0801088 */
+	unsigned int	swrstc1b;	/* offset: 0x8C, addr: 0xF080108C */
+	unsigned int	swrstc2b;	/* offset: 0x90, addr: 0xF0801090 */
+	unsigned int	swrstc3b;	/* offset: 0x94, addr: 0xF0801094 */
+	unsigned int	tiprstcb;	/* offset: 0x98, addr: 0xF0801098 */
+	unsigned int	corstcb;	/* offset: 0x9C, addr: 0xF080109C */
+	unsigned int	ipsrstdis1;	/* offset: 0xA0, addr: 0xF08010A0 */
+	unsigned int	ipsrstdis2;	/* offset: 0xA4, addr: 0xF08010A4 */
+	unsigned int	ipsrstdis3;	/* offset: 0xA8, addr: 0xF08010A8 */
+	unsigned int	ipsrstdis4;	/* offset: 0xAC, addr: 0xF08010AC */
+	unsigned char	res2[0x10];	/* offset: 0xB0, addr: 0xF08010B0 (CLKENDIS1-4 not used) */
+	unsigned int	thrtl_cnt;	/* offset: 0xC0, addr: 0xF08010C0 */
 };
 
 #endif /* __ARBEL_CLOCK_H_ */
--- include/drivers/nuvoton/npcm845x/npcm845x_gcr.h
+++ include/drivers/nuvoton/npcm845x/npcm845x_gcr.h
@@ -7,121 +7,122 @@
 #ifndef __NPCM845x_GCR_H_
 #define __NPCM845x_GCR_H_
 
+/* GCR_BA = 0xF0800000 */
 struct npcm845x_gcr {
-	unsigned int pdid;
-	unsigned int pwron;
-	unsigned int swstrps;
-	unsigned int rsvd1[2];
-	unsigned int miscpe;
-	unsigned int spldcnt;
-	unsigned int rsvd2[1];
-	unsigned int flockr2;
-	unsigned int flockr3;
-	unsigned int rsvd3[3];
-	unsigned int a35_mode;
-	unsigned int spswc;
-	unsigned int intcr;
-	unsigned int intsr;
-	unsigned int obscr1;
-	unsigned int obsdr1;
-	unsigned int rsvd4[1];
-	unsigned int hifcr;
-	unsigned int rsvd5[3];
-	unsigned int intcr2;
-	unsigned int rsvd6[1];
-	unsigned int srcnt;
-	unsigned int ressr;
-	unsigned int rlockr1;
-	unsigned int flockr1;
-	unsigned int dscnt;
-	unsigned int mdlr;
-	unsigned int scrpad_c;
-	unsigned int scrpad_b;
-	unsigned int rsvd7[4];
-	unsigned int daclvlr;
-	unsigned int intcr3;
-	unsigned int pcirctl;
-	unsigned int rsvd8[2];
-	unsigned int vsintr;
-	unsigned int rsvd9[1];
-	unsigned int sd2sur1;
-	unsigned int sd2sur2;
-	unsigned int sd2irv3;
-	unsigned int intcr4;
-	unsigned int obscr2;
-	unsigned int obsdr2;
-	unsigned int rsvd10[5];
-	unsigned int i2csegsel;
-	unsigned int i2csegctl;
-	unsigned int vsrcr;
-	unsigned int mlockr;
-	unsigned int rsvd11[8];
-	unsigned int etsr;
-	unsigned int dft1r;
-	unsigned int dft2r;
-	unsigned int dft3r;
-	unsigned int edffsr;
-	unsigned int rsvd12[1];
-	unsigned int intcrpce3;
-	unsigned int intcrpce2;
-	unsigned int intcrpce0;
-	unsigned int intcrpce1;
-	unsigned int dactest;
-	unsigned int scrpad;
-	unsigned int usb1phyctl;
-	unsigned int usb2phyctl;
-	unsigned int usb3phyctl;
-	unsigned int intsr2;
-	unsigned int intcrpce2b;
-	unsigned int intcrpce0b;
-	unsigned int intcrpce1b;
-	unsigned int intcrpce3b;
-	unsigned int rsvd13[4];
-	unsigned int intcrpce2c;
-	unsigned int intcrpce0c;
-	unsigned int intcrpce1c;
-	unsigned int intcrpce3c;
-	unsigned int rsvd14[40];
-	unsigned int sd2irv4;
-	unsigned int sd2irv5;
-	unsigned int sd2irv6;
-	unsigned int sd2irv7;
-	unsigned int sd2irv8;
-	unsigned int sd2irv9;
-	unsigned int sd2irv10;
-	unsigned int sd2irv11;
-	unsigned int rsvd15[8];
-	unsigned int mfsel1;
-	unsigned int mfsel2;
-	unsigned int mfsel3;
-	unsigned int mfsel4;
-	unsigned int mfsel5;
-	unsigned int mfsel6;
-	unsigned int mfsel7;
-	unsigned int rsvd16[1];
-	unsigned int mfsel_lk1;
-	unsigned int mfsel_lk2;
-	unsigned int mfsel_lk3;
-	unsigned int mfsel_lk4;
-	unsigned int mfsel_lk5;
-	unsigned int mfsel_lk6;
-	unsigned int mfsel_lk7;
-	unsigned int rsvd17[1];
-	unsigned int mfsel_set1;
-	unsigned int mfsel_set2;
-	unsigned int mfsel_set3;
-	unsigned int mfsel_set4;
-	unsigned int mfsel_set5;
-	unsigned int mfsel_set6;
-	unsigned int mfsel_set7;
-	unsigned int rsvd18[1];
-	unsigned int mfsel_clr1;
-	unsigned int mfsel_clr2;
-	unsigned int mfsel_clr3;
-	unsigned int mfsel_clr4;
-	unsigned int mfsel_clr5;
-	unsigned int mfsel_clr6;
-	unsigned int mfsel_clr7;
+	unsigned int pdid;		/* offset: 0x000, addr: 0xF0800000 */
+	unsigned int pwron;		/* offset: 0x004, addr: 0xF0800004 */
+	unsigned int swstrps;		/* offset: 0x008, addr: 0xF0800008 */
+	unsigned int rsvd1[2];		/* offset: 0x00C, addr: 0xF080000C */
+	unsigned int miscpe;		/* offset: 0x014, addr: 0xF0800014 */
+	unsigned int spldcnt;		/* offset: 0x018, addr: 0xF0800018 */
+	unsigned int rsvd2[1];		/* offset: 0x01C, addr: 0xF080001C */
+	unsigned int flockr2;		/* offset: 0x020, addr: 0xF0800020 */
+	unsigned int flockr3;		/* offset: 0x024, addr: 0xF0800024 */
+	unsigned int rsvd3[3];		/* offset: 0x028, addr: 0xF0800028 (VSNSCALR at 0x028 not used) */
+	unsigned int a35_mode;		/* offset: 0x034, addr: 0xF0800034 */
+	unsigned int spswc;		/* offset: 0x038, addr: 0xF0800038 */
+	unsigned int intcr;		/* offset: 0x03C, addr: 0xF080003C */
+	unsigned int intsr;		/* offset: 0x040, addr: 0xF0800040 */
+	unsigned int obscr1;		/* offset: 0x044, addr: 0xF0800044 */
+	unsigned int obsdr1;		/* offset: 0x048, addr: 0xF0800048 */
+	unsigned int rsvd4[1];		/* offset: 0x04C, addr: 0xF080004C */
+	unsigned int hifcr;		/* offset: 0x050, addr: 0xF0800050 */
+	unsigned int rsvd5[3];		/* offset: 0x054, addr: 0xF0800054 */
+	unsigned int intcr2;		/* offset: 0x060, addr: 0xF0800060 */
+	unsigned int rsvd6[1];		/* offset: 0x064, addr: 0xF0800064 */
+	unsigned int srcnt;		/* offset: 0x068, addr: 0xF0800068 */
+	unsigned int ressr;		/* offset: 0x06C, addr: 0xF080006C */
+	unsigned int rlockr1;		/* offset: 0x070, addr: 0xF0800070 */
+	unsigned int flockr1;		/* offset: 0x074, addr: 0xF0800074 */
+	unsigned int dscnt;		/* offset: 0x078, addr: 0xF0800078 */
+	unsigned int mdlr;		/* offset: 0x07C, addr: 0xF080007C */
+	unsigned int scrpad_c;		/* offset: 0x080, addr: 0xF0800080 */
+	unsigned int scrpad_b;		/* offset: 0x084, addr: 0xF0800084 */
+	unsigned int rsvd7[4];		/* offset: 0x088, addr: 0xF0800088 */
+	unsigned int daclvlr;		/* offset: 0x098, addr: 0xF0800098 */
+	unsigned int intcr3;		/* offset: 0x09C, addr: 0xF080009C */
+	unsigned int pcirctl;		/* offset: 0x0A0, addr: 0xF08000A0 */
+	unsigned int rsvd8[2];		/* offset: 0x0A4, addr: 0xF08000A4 */
+	unsigned int vsintr;		/* offset: 0x0AC, addr: 0xF08000AC */
+	unsigned int rsvd9[1];		/* offset: 0x0B0, addr: 0xF08000B0 */
+	unsigned int sd2sur1;		/* offset: 0x0B4, addr: 0xF08000B4 */
+	unsigned int sd2sur2;		/* offset: 0x0B8, addr: 0xF08000B8 */
+	unsigned int sd2irv3;		/* offset: 0x0BC, addr: 0xF08000BC */
+	unsigned int intcr4;		/* offset: 0x0C0, addr: 0xF08000C0 */
+	unsigned int obscr2;		/* offset: 0x0C4, addr: 0xF08000C4 */
+	unsigned int obsdr2;		/* offset: 0x0C8, addr: 0xF08000C8 */
+	unsigned int rsvd10[5];		/* offset: 0x0CC, addr: 0xF08000CC */
+	unsigned int i2csegsel;		/* offset: 0x0E0, addr: 0xF08000E0 */
+	unsigned int i2csegctl;		/* offset: 0x0E4, addr: 0xF08000E4 */
+	unsigned int vsrcr;		/* offset: 0x0E8, addr: 0xF08000E8 */
+	unsigned int mlockr;		/* offset: 0x0EC, addr: 0xF08000EC */
+	unsigned int rsvd11[8];		/* offset: 0x0F0, addr: 0xF08000F0 */
+	unsigned int etsr;		/* offset: 0x110, addr: 0xF0800110 */
+	unsigned int dft1r;		/* offset: 0x114, addr: 0xF0800114 */
+	unsigned int dft2r;		/* offset: 0x118, addr: 0xF0800118 */
+	unsigned int dft3r;		/* offset: 0x11C, addr: 0xF080011C */
+	unsigned int edffsr;		/* offset: 0x120, addr: 0xF0800120 */
+	unsigned int rsvd12[1];		/* offset: 0x124, addr: 0xF0800124 */
+	unsigned int intcrpce3;		/* offset: 0x128, addr: 0xF0800128 */
+	unsigned int intcrpce2;		/* offset: 0x12C, addr: 0xF080012C */
+	unsigned int intcrpce0;		/* offset: 0x130, addr: 0xF0800130 */
+	unsigned int intcrpce1;		/* offset: 0x134, addr: 0xF0800134 */
+	unsigned int dactest;		/* offset: 0x138, addr: 0xF0800138 */
+	unsigned int scrpad;		/* offset: 0x13C, addr: 0xF080013C */
+	unsigned int usb1phyctl;	/* offset: 0x140, addr: 0xF0800140 */
+	unsigned int usb2phyctl;	/* offset: 0x144, addr: 0xF0800144 */
+	unsigned int usb3phyctl;	/* offset: 0x148, addr: 0xF0800148 */
+	unsigned int intsr2;		/* offset: 0x14C, addr: 0xF080014C */
+	unsigned int intcrpce2b;	/* offset: 0x150, addr: 0xF0800150 */
+	unsigned int intcrpce0b;	/* offset: 0x154, addr: 0xF0800154 */
+	unsigned int intcrpce1b;	/* offset: 0x158, addr: 0xF0800158 */
+	unsigned int intcrpce3b;	/* offset: 0x15C, addr: 0xF080015C */
+	unsigned int rsvd13[4];		/* offset: 0x160, addr: 0xF0800160 */
+	unsigned int intcrpce2c;	/* offset: 0x170, addr: 0xF0800170 */
+	unsigned int intcrpce0c;	/* offset: 0x174, addr: 0xF0800174 */
+	unsigned int intcrpce1c;	/* offset: 0x178, addr: 0xF0800178 */
+	unsigned int intcrpce3c;	/* offset: 0x17C, addr: 0xF080017C */
+	unsigned int rsvd14[40];	/* offset: 0x180, addr: 0xF0800180 */
+	unsigned int sd2irv4;		/* offset: 0x220, addr: 0xF0800220 */
+	unsigned int sd2irv5;		/* offset: 0x224, addr: 0xF0800224 */
+	unsigned int sd2irv6;		/* offset: 0x228, addr: 0xF0800228 */
+	unsigned int sd2irv7;		/* offset: 0x22C, addr: 0xF080022C */
+	unsigned int sd2irv8;		/* offset: 0x230, addr: 0xF0800230 */
+	unsigned int sd2irv9;		/* offset: 0x234, addr: 0xF0800234 */
+	unsigned int sd2irv10;		/* offset: 0x238, addr: 0xF0800238 */
+	unsigned int sd2irv11;		/* offset: 0x23C, addr: 0xF080023C */
+	unsigned int rsvd15[8];		/* offset: 0x240, addr: 0xF0800240 */
+	unsigned int mfsel1;		/* offset: 0x260, addr: 0xF0800260 */
+	unsigned int mfsel2;		/* offset: 0x264, addr: 0xF0800264 */
+	unsigned int mfsel3;		/* offset: 0x268, addr: 0xF0800268 */
+	unsigned int mfsel4;		/* offset: 0x26C, addr: 0xF080026C */
+	unsigned int mfsel5;		/* offset: 0x270, addr: 0xF0800270 */
+	unsigned int mfsel6;		/* offset: 0x274, addr: 0xF0800274 */
+	unsigned int mfsel7;		/* offset: 0x278, addr: 0xF0800278 */
+	unsigned int rsvd16[1];		/* offset: 0x27C, addr: 0xF080027C */
+	unsigned int mfsel_lk1;		/* offset: 0x280, addr: 0xF0800280 */
+	unsigned int mfsel_lk2;		/* offset: 0x284, addr: 0xF0800284 */
+	unsigned int mfsel_lk3;		/* offset: 0x288, addr: 0xF0800288 */
+	unsigned int mfsel_lk4;		/* offset: 0x28C, addr: 0xF080028C */
+	unsigned int mfsel_lk5;		/* offset: 0x290, addr: 0xF0800290 */
+	unsigned int mfsel_lk6;		/* offset: 0x294, addr: 0xF0800294 */
+	unsigned int mfsel_lk7;		/* offset: 0x298, addr: 0xF0800298 */
+	unsigned int rsvd17[1];		/* offset: 0x29C, addr: 0xF080029C */
+	unsigned int mfsel_set1;	/* offset: 0x2A0, addr: 0xF08002A0 */
+	unsigned int mfsel_set2;	/* offset: 0x2A4, addr: 0xF08002A4 */
+	unsigned int mfsel_set3;	/* offset: 0x2A8, addr: 0xF08002A8 */
+	unsigned int mfsel_set4;	/* offset: 0x2AC, addr: 0xF08002AC */
+	unsigned int mfsel_set5;	/* offset: 0x2B0, addr: 0xF08002B0 */
+	unsigned int mfsel_set6;	/* offset: 0x2B4, addr: 0xF08002B4 */
+	unsigned int mfsel_set7;	/* offset: 0x2B8, addr: 0xF08002B8 */
+	unsigned int rsvd18[1];		/* offset: 0x2BC, addr: 0xF08002BC */
+	unsigned int mfsel_clr1;	/* offset: 0x2C0, addr: 0xF08002C0 */
+	unsigned int mfsel_clr2;	/* offset: 0x2C4, addr: 0xF08002C4 */
+	unsigned int mfsel_clr3;	/* offset: 0x2C8, addr: 0xF08002C8 */
+	unsigned int mfsel_clr4;	/* offset: 0x2CC, addr: 0xF08002CC */
+	unsigned int mfsel_clr5;	/* offset: 0x2D0, addr: 0xF08002D0 */
+	unsigned int mfsel_clr6;	/* offset: 0x2D4, addr: 0xF08002D4 */
+	unsigned int mfsel_clr7;	/* offset: 0x2D8, addr: 0xF08002D8 */
 };
 
 #endif
